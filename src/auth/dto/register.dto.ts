80c962c333cfd2969f62c82f5d5ed2aecfbc23c0
abdelilah <abdouajjouqa@gmail.com>
2025-12-22 11:29:17 +0100

    feat: add DTOs for user registration and login, and include class-validator dependency

---
+++ src/auth/dto/register.dto.ts
@@ -0,0 +1,19 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class RegisterDto {
+    @IsString()
+    @IsNotEmpty()
+    firstName: string;
+
+    @IsString()
+    @IsNotEmpty()
+    lastName: string;
+
+    @IsEmail()
+    @IsNotEmpty()
+    @IsString()
+    email: string;
+
+    @IsNotEmpty()
+    password: string;
+}
